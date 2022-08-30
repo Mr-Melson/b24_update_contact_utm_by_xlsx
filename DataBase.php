0828e820a4dc8ef5bae220fb2eb132a01f27dc0d
Mel-SON <omelyanchenko.semen@yandex.ru>
2022-08-30 13:03:59 +0300

    Убирает лишнее

--- DataBase.php
+++ DataBase.php
@@ -1,26 +1,20 @@
 <?
 require_once (__DIR__ . '/db_settings.php');
 
-// Create connection
 $contacts_utm_database = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($contacts_utm_database->connect_error) {
     die("Connection failed: " . $contacts_utm_database->connect_error);
 }
 
-// Check exist
 $result = $contacts_utm_database->query("SHOW TABLES LIKE 'Contacts_UTM'");
 
 if ($result->num_rows == 1) {
-
-    // echo "Table exists";
     return;
 } else {
 
     echo "Table does not exist";
 
-    // sql to create table
     $sql = "CREATE TABLE Contacts_UTM (
         id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         email VARCHAR(30) NOT NULL,
@@ -38,4 +32,3 @@ if ($result->num_rows == 1) {
         echo "<br>Error creating table: " . $contacts_utm_database->error;
     }
 }
-// $conn->close();
